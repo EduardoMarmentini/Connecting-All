1b5d70c1f956efb3b4fb706c741fb8c8b38b93b7
EduardoDevword <eduardomarmentini.dev@gmail.com>
2023-11-27 16:58:46 -0300

    instalado os pacotes para realizar a conexão com o banco de dados SQL Server

--- ControleDeContatos/ControleDeContatos/obj/Debug/net7.0/ControleDeContatos.AssemblyInfo.cs
+++ ControleDeContatos/ControleDeContatos/obj/Debug/net7.0/ControleDeContatos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85d88217d9c5dc3f900b1d17d6665139bfcfcbb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ControleDeContatos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
