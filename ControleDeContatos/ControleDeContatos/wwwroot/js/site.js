edc884cfc909ab1bd49c69f6ad4108f9b49fed00
EduardoDevword <eduardomarmentini.dev@gmail.com>
2023-12-07 16:08:56 -0300

    Finalizado a parte de paginação e busca dos dados nas demais tabelas, partindo para criação do crud de registro de usuarios no sistema

--- ControleDeContatos/ControleDeContatos/wwwroot/js/site.js
+++ ControleDeContatos/ControleDeContatos/wwwroot/js/site.js
@@ -1,9 +1,33 @@
 ﻿$(document).ready(function () {
-
-    // Some com o a mensagem de alert
-    $(".btn-close").click(function()  {
-        $(".alert").hide("hide");
-    })
+    // Cria o objeto que ira receber os metodos da datatable de paginação e busca nas tabelas das view
+    $('#table-contatos').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": "Mostrar _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty": "Mostrar 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total registros)",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Proximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Ultimo"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
 
     const inputFile = $("#picture_upload");
     const pictureImage = $(".exibe_foto");
